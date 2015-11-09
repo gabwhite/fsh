a6e3130759e063b163d3b1b86234498da0c4d322
Breen <breen.young@gmail.com>
2015-11-08 20:36:59 -0400

    admin and searrch

---
+++ fshweb/resources/views/profile/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+
+@section('css')
+
+@endsection
+
+@section('sidebar')
+    @parent
+
+
+@endsection
+
+@section('content')
+
+    Currently logged in user: {{Auth::user()->name}} ({{Auth::user()->email}})
+    <br/><br/>
+
+    @if(Auth::user()->hasRole('admin'))
+        You have <a href="{{url('/admin')}}">administrative</a> access
+    @endif
+
+    <hr/>
+    More Coming Soon
+
+@endsection
+
+@section('scripts')
+
+
+@endsection
