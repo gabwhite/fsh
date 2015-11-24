bcddf40361743e58b8f394822820e6baabc36718
Breen <breen.young@gmail.com>
2015-11-24 13:02:08 -0400

    change column ordering

--- fshweb/database/migrations/2015_11_24_161009_alter_user_profiles_table.php
+++ fshweb/database/migrations/2015_11_24_161009_alter_user_profiles_table.php
@@ -14,14 +14,14 @@ class AlterUserProfilesTable extends Migration
     {
         Schema::table('user_profiles', function (Blueprint $table)
         {
-            $table->string('company', 200)->nullable();
-            $table->integer('country')->unsigned()->nullable();
-            $table->integer('state_province')->unsigned()->nullable();
-            $table->string('city', 200)->nullable();
-            $table->string('zip_postal', 50)->nullable();
-            $table->string('contact_name', 200)->nullable();
-            $table->string('contact_phone', 200)->nullable();
-            $table->string('logo_image_path', 200)->nullable();
+            $table->string('logo_image_path', 200)->nullable()->after('vendor_id');
+            $table->string('contact_phone', 200)->nullable()->after('vendor_id');
+            $table->string('contact_name', 200)->nullable()->after('vendor_id');
+            $table->string('zip_postal', 50)->nullable()->after('vendor_id');
+            $table->string('city', 200)->nullable()->after('vendor_id');
+            $table->integer('state_province')->unsigned()->nullable()->after('vendor_id');
+            $table->integer('country')->unsigned()->nullable()->after('vendor_id');
+            $table->string('company', 200)->nullable()->after('vendor_id');
         });
     }
 
