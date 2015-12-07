0b5bbc768d3f3880620c40691e720d9a4f1646de
Breen <breen.young@gmail.com>
2015-12-06 20:35:23 -0400

    add fillables

--- fshweb/app/Models/UserProduct.php
+++ fshweb/app/Models/UserProduct.php
@@ -8,6 +8,18 @@ class UserProduct extends Model
 {
     protected $table = 'user_products';
 
+    protected $fillable =
+        [
+            'user_id',
+            'uniquekey', 'name', 'description', 'brand',
+            'pack', 'calc_size', 'mpc', 'broker_contact', 'allergen_disclaimer',
+            'features_benefits', 'ingredient_deck', 'uom',
+            'gtin', 'calories', 'calories_from_fat', 'saturated_fats', 'total_fat',
+            'sodium', 'fibre', 'sugar', 'protein', 'carbs', 'net_weight', 'gross_weight',
+            'tare_weight', 'is_halal', 'is_organic', 'is_kosher', 'serving_size', 'calculation_size_uom',
+            'serving_size_uom', 'preparation', 'size', 'vendor_logo', 'product_image', 'pos_pdf', 'published'
+        ];
+
     public function allergens()
     {
         return $this->belongsToMany('App\Models\Allergen', 'user_products_allergens', 'product_id', 'allergen_id');
