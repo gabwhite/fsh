ebcfd09f54f4b37e1665f1073895fc42c64f101d
Breen <breen.young@gmail.com>
2016-01-17 00:27:36 -0400

    implement brands on public vendor page

--- fshweb/app/Http/Controllers/VendorController.php
+++ fshweb/app/Http/Controllers/VendorController.php
@@ -29,7 +29,7 @@ class VendorController extends Controller
 
     public function detail($id)
     {
-        $vendor = $this->dataAccess->getVendor($id, null, ['country', 'stateProvince']);
+        $vendor = $this->dataAccess->getVendor($id, null, ['country', 'stateProvince', 'brands']);
 
         return view('vendor.view')->with('profile', $vendor);
     }
--- fshweb/resources/views/vendor/view.blade.php
+++ fshweb/resources/views/vendor/view.blade.php
@@ -105,18 +105,10 @@
                     <div class="col-xs-12 well">
                         <div class="flexslider">
                           <ul class="slides">
-                            <li style="background: url(../public/img/slider/campbells-brand-logo.png); background-repeat: no-repeat; background-position: center center;">
-                              
-                            </li>
-
-                            <li style="background: url(../public/img/slider/pepperidgefarm-logo.png); background-repeat: no-repeat; background-position: center center;">
-                            </li>
-
-                            <li style="background: url(../public/img/slider/ROLAND-LOGO-BANNER-SIZE.png); background-repeat: no-repeat; background-position: center center;">
-                            </li>
-                            <li style="background: url(../public/img/slider/ROLAND-LOGO-BANNER-SIZE.png); background-repeat: no-repeat; background-position: center center;">
-                        
-                            </li>
+                          @foreach($profile->brands as $b)
+                              <li style="background: url('{{url(config('app.vendor_storage'))}}/{{$b->logo_image_path}}'); background-repeat: no-repeat; background-position: center center;">
+                              </li>
+                          @endforeach
                             <!-- items mirrored twice, total of 12 -->
                           </ul>
                         </div>
