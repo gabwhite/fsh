8495b8a836d4b62e22327814a911877cb6544399
Breen <breen.young@gmail.com>
2016-01-18 19:44:18 -0400

    fix missing avatar on edit profile page

--- fshweb/resources/views/profile/edit.blade.php
+++ fshweb/resources/views/profile/edit.blade.php
@@ -10,8 +10,8 @@
 <section class='clearfix container-wrap profile-head'>
     <div class="container">
         <div class="col-xs-12 col-md-4">
-          @if(isset($avatarFilename))
-              <img id="imgCurrentAvatar" src="{{url(config('app.avatar_storage') . '/' . $avatarFilename)}}" title="{{trans('ui.user_label_currentavatar')}}" width="200" height="200"/>
+          @if($profile->avatar_image_path)
+              <img id="imgCurrentAvatar" src="{{url(config('app.avatar_storage') . '/' . $profile->avatar_image_path)}}" title="{{trans('ui.user_label_currentavatar')}}" width="200" height="200"/>
           @else
               <img id="imgCurrentAvatar" src="{{url(config('app.avatar_none'))}}" title="{{trans('ui.user_label_noavatar')}}" width="200" height="200"/>
           @endif      
