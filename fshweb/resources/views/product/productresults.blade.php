2268173c814e1a45b8cc01642bab3da69940cb86
Gab White <gabwhite@Gabs-MacBook-Pro.local>
2016-01-20 10:13:54 -0400

    styling to the search page

--- fshweb/resources/views/product/productresults.blade.php
+++ fshweb/resources/views/product/productresults.blade.php
@@ -16,6 +16,18 @@
         <div class="col-xs-9">
             <a href="{{url('product/detail', $p->id)}}">{{$p->name}}</a>
             <span class="brand">{{$p->brand}}</span>
+
+            <p>{{$p->description}}</p>
+            
+            <div class="col-xs-12 search-details">
+                <p>Pack:</p>
+
+                <p>Size:</p>
+
+                <p>Unit of Measure:</p>
+
+                <p>Product Code:</p>
+            </div>
         </div>
     </div>
 
--- fshweb/resources/views/product/search.blade.php
+++ fshweb/resources/views/product/search.blade.php
@@ -86,30 +86,6 @@
                         <div id="product_list">
                         </div>
 
-                        <div class="row">
-                            <div class="col-xs-12">
-                               <nav class="text-center">
-                                 <ul class="pagination">
-                                    <p>Page</p>
-                                   <li>
-                                     <a href="#" aria-label="Previous">
-                                       <span aria-hidden="true">&laquo;</span>
-                                     </a>
-                                   </li>
-                                   <li><a href="#">1</a></li>
-                                   <li><a href="#">2</a></li>
-                                   <li><a href="#">3</a></li>
-                                   <li><a href="#">4</a></li>
-                                   <li><a href="#">5</a></li>
-                                   <li>
-                                     <a href="#" aria-label="Next">
-                                       <span aria-hidden="true">&raquo;</span>
-                                     </a>
-                                   </li>
-                                 </ul>
-                               </nav> 
-                            </div>
-                        </div>
                     </div>
                 </div>
 
