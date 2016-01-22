5f4f932a3f4ee7a35f39933774b5b2774293e03b
Breen <breen.young@gmail.com>
2016-01-22 16:31:22 -0400

    fix broken cancel button

--- fshweb/resources/views/profile/edit.blade.php
+++ fshweb/resources/views/profile/edit.blade.php
@@ -81,7 +81,7 @@
         <div class="row btn-row">
             <div class="col-xs-12">
                 <input type="submit" value="{{trans('ui.button_update')}}" class="btn-primary"/>
-                <a href="{{url('/profile')}}"><button class="btn">{{trans('ui.button_cancel')}}</button></a>
+                <button class="btn"><a href="{{url('/profile')}}">{{trans('ui.button_cancel')}}</a></button>
             </div>
         </div>
 
