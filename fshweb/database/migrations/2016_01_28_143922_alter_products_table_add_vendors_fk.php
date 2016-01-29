cb8f8006b0fbc5f5679d2a0398158f8724f677c1
Breen <breen.young@gmail.com>
2016-01-28 20:59:42 -0400

    fixed wrong class name

--- fshweb/database/migrations/2016_01_28_143922_alter_products_table_add_vendors_fk.php
+++ fshweb/database/migrations/2016_01_28_143922_alter_products_table_add_vendors_fk.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterProductsTableAddUsersFk extends Migration
+class AlterProductsTableAddVendorsFk extends Migration
 {
     /**
      * Run the migrations.
