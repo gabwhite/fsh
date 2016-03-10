7fba83c09c9190ba54650742fb69053d8b01fee3
Gab White <gabwhite@Gabs-MacBook-Pro.local>
2016-03-10 11:13:24 -0400

    styling to the pricing tables on vendor page

--- fshweb/resources/views/welcome.blade.php
+++ fshweb/resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 		<button class="btn center-block play-vid">Play</button>
 
 		<h2 class="page-subhead">Every day Chefs are looking to learn more about products and what is available on the market. Let them see everything you have to offer.</h2>
-		<a href="/auth/register"><button class="btn-primary">Sign Up</button></a>
+		<a href="/auth/vendorregister"><button class="btn-primary">Sign Up</button></a>
     </div>
 </section>
 
@@ -29,77 +29,85 @@
 @section('content')
 
 <div class="row">
-
+	<div class="col-xs-12 text-center">
+	</div>
     <div class="col-xs-12 text-center">
-		<h2>A sourcing library for Chefs –  A lead generation system for you!</h2>
+		<h2 class="vendor-subhead">A sourcing library for Chefs –  A lead generation system for you!</h2>
 		<h3>Show your products and grow your brand.</h3>
 		
-		<div class="well col-xs-12 col-sm-4">
-			<div class="col-xs-12 table-header">
-				<h3>Starter</h3>
-				<p>Free</p>
+		<div class="col-xs-12 pricing-options">
+			<div class="price-table col-xs-12 col-sm-4">
+				<div class="col-xs-12 table-header">
+					<h3>Starter</h3>
+					<p>Free</p>
+				</div>
+
+				<div class="col-xs-12 table-body">
+					<p>150 Products</p>
+					<p>Vendor Profile Page</p>
+					<p>Item Description</p>
+					<p>Product Specs</p>
+					<p>Ingredient Deck</p>
+					<p>Benefit Statement</p>
+					<p>Preparation Instructions</p>
+
+					<a href="/auth/vendorregister"><button class="btn-primary">Sign Up</button></a>
+				</div>
+			</div>
+		
+			<div class="price-table col-xs-12 col-sm-4 premium">
+				<div class="col-xs-12 pre-header">
+					<h2>Free 1 Year Subscription</h2>
+				</div>
+				<div class="col-xs-12 table-header ">
+					<h3>Premium</h3>
+					<p>$39 / Month</p>
+				</div>
+
+				<div class="col-xs-12 table-body">
+					<p>150 Products</p>
+					<p>Vendor Profile Page</p>
+					<p>Item Description</p>
+					<p>Product Specs</p>
+					<p>Ingredient Deck</p>
+					<p>Benefit Statement</p>
+					<p>Preparation Instructions</p>
+					<p>Nutrionals</p>
+					<p>Allergens</p>
+					<p>Samples Request Button</p>
+					<p>Monthly Portfolio Activity Reports</p>
+
+					<a href="/auth/vendorregister"><button class="btn-primary">Sign Up</button></a>
+				</div>
 			</div>
 
-			<div class="col-xs-12 table-body">
-				<p>150 Products</p>
-				<p>Vendor Profile Page</p>
-				<p>Item Description</p>
-				<p>Product Specs</p>
-				<p>Ingredient Deck</p>
-				<p>Benefit Statement</p>
-				<p>Preparation Instructions</p>
+			<div class="price-table col-xs-12 col-sm-4">
+				<div class="col-xs-12 table-header">
+					<h3>Enterprise</h3>
+					<p>$199 / Month</p>
+				</div>
 
-				<a href="/auth/register"><button class="btn-primary">Sign Up</button></a>
-			</div>
-		</div>
-		<div class="well col-xs-12 col-sm-4">
-			<div class="col-xs-12 table-header">
-				<h3>Premium</h3>
-				<p>$39 / Month</p>
-			</div>
+				<div class="col-xs-12 table-body">
+					<p>150 Products</p>
+					<p>Vendor Profile Page</p>
+					<p>Item Description</p>
+					<p>Product Specs</p>
+					<p>Ingredient Deck</p>
+					<p>Benefit Statement</p>
+					<p>Preparation Instructions</p>
+					<p>Nutrionals</p>
+					<p>Allergens</p>
+					<p>Samples Request Button</p>
+					<p>Monthly Portfolio Activity Reports</p>
+					<p>Priority Placement</p>
+					<p>Featured Products on Landing Page</p>
+					<p>Member Newsletter Marketing</p>
+					<p>Vendor Blog / Article Publishing</p>
 
-			<div class="col-xs-12 table-body">
-				<p>150 Products</p>
-				<p>Vendor Profile Page</p>
-				<p>Item Description</p>
-				<p>Product Specs</p>
-				<p>Ingredient Deck</p>
-				<p>Benefit Statement</p>
-				<p>Preparation Instructions</p>
-				<p>Nutrionals</p>
-				<p>Allergens</p>
-				<p>Samples Request Button</p>
-				<p>Monthly Portfolio Activity Reports</p>
-
-				<a href="/auth/register"><button class="btn-primary">Sign Up</button></a>
+					<a href="/auth/vendorregister"><button class="btn-primary">Sign Up</button></a>
+				</div>
 			</div>
-		</div>
-		<div class="well col-xs-12 col-sm-4">
-			<div class="col-xs-12 table-header">
-				<h3>Enterprise</h3>
-				<p>$199 / Month</p>
-			</div>
-
-			<div class="col-xs-12 table-body">
-				<p>150 Products</p>
-				<p>Vendor Profile Page</p>
-				<p>Item Description</p>
-				<p>Product Specs</p>
-				<p>Ingredient Deck</p>
-				<p>Benefit Statement</p>
-				<p>Preparation Instructions</p>
-				<p>Nutrionals</p>
-				<p>Allergens</p>
-				<p>Samples Request Button</p>
-				<p>Monthly Portfolio Activity Reports</p>
-				<p>Priority Placement</p>
-				<p>Featured Products on Landing Page</p>
-				<p>Member Newsletter Marketing</p>
-				<p>Vendor Blog / Article Publishing</p>
-
-				<a href="/auth/register"><button class="btn-primary">Sign Up</button></a>
-			</div>
-		</div>
+		</div> <!-- end of pricing options	 -->
     </div>
 
 </div>
